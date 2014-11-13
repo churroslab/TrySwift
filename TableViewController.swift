d12a2e3f869bc3ac9420aee378fabe8896773d09
churroslab <charles@coutunadeau.com>
2014-11-12 21:21:14 -0500

    My first try at Swift. A table view controller that displays major cities

---
+++ TableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TableViewController.swift
+//  TrySwift
+//
+//  Created by Charles Coutu-Nadeau on 11/12/14.
+//  Copyright (c) 2014 churroslab. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    // private vs public . var (dynamic i.e. mutable) vs let (static)
+    private let cities = ["Montreal", "New York", "Paris", "London", "San Francisco", "Moscow", "Shanghai"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self.cities.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("prototypeCell", forIndexPath: indexPath) as UITableViewCell
+
+        cell.textLabel.text = self.cities[indexPath.row]
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
